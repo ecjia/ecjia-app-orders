1609af722bf4f19d61bf2fea4074ab90285966e2
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-05-20 13:16:22 +0800

    同步更新收银台接口

--- modules/admin/orders/list_module.class.php
+++ modules/admin/orders/list_module.class.php
@@ -16,91 +16,96 @@ class list_module implements ecjia_interface {
 		if (is_ecjia_error($result)) {
 			EM_Api::outPut($result);
 		}
-		$type = _POST('type', 'await_pay');
-		$keywords = _POST('keywords');
-		$size = EM_Api::$pagination['count'];
-		$page = EM_Api::$pagination['page'];
+		$type		= _POST('type', 'await_pay');
+		$keywords	= _POST('keywords');
+		$size		= EM_Api::$pagination['count'];
+		$page		= EM_Api::$pagination['page'];
+		
+		$device = _POST('device', array());
+		$device_code = isset($device['code']) ? $device['code'] : '';
 		
 		$where = array();
 		$order_query = RC_Loader::load_app_class('order_query', 'orders');
-		
-// 		await_pay/待付款，await_ship/待发货，shipped/已发货，finished/已完成，refund/退款(退货)，closed(invalid)/已关闭
-		switch ($type) {
-			case 'await_pay':
-				$where = $order_query->order_await_pay('oi.');
-				break;
-			case 'await_ship':
-				$where = $order_query->order_await_ship('oi.');
-				break;
-			case 'shipped':
-				$where = $order_query->order_shipped('oi.');
-				break;
-			case 'finished':
-				$where = $order_query->order_finished('oi.');
-				break;
-			case 'refund':
-				$where = $order_query->order_refund('oi.');
-				break;
-			case 'closed' :
-				$where = array_merge($order_query->order_invalid('oi.'),$order_query->order_canceled('oi.'));
-				break;
-		}
-	
-		if ( !empty($keywords)) {
-			$where[] = "( oi.order_sn like '%".$keywords."%' or oi.consignee like '%".$keywords."%' )";
-		}
 		$db = RC_Loader::load_app_model('order_info_model', 'orders');
 		$db_view = RC_Loader::load_app_model('order_info_viewmodel', 'orders');
+		if ($device_code != '8001') {
+			switch ($type) {
+				case 'await_pay':
+					$where = $order_query->order_await_pay('oi.');
+					break;
+				case 'await_ship':
+					$where = $order_query->order_await_ship('oi.');
+					break;
+				case 'shipped':
+					$where = $order_query->order_shipped('oi.');
+					break;
+				case 'finished':
+					$where = $order_query->order_finished('oi.');
+					break;
+				case 'refund':
+					$where = $order_query->order_refund('oi.');
+					break;
+				case 'closed' :
+					$where = array_merge($order_query->order_invalid('oi.'),$order_query->order_canceled('oi.'));
+					break;
+			}
 		
-		$db_orderinfo_view = RC_Loader::load_app_model('order_info_viewmodel', 'orders');
-		$result = ecjia_app::validate_application('seller');
-		if (!is_ecjia_error($result)) {
-			$db_orderinfo_view->view = array(
-					'order_info' => array(
-							'type'	=> Component_Model_View::TYPE_LEFT_JOIN,
-							'alias'	=> 'oii',
-							'on'	=> 'oi.order_id = oii.main_order_id'
-					),
-					'order_goods' => array(
-							'type'	=> Component_Model_View::TYPE_LEFT_JOIN,
-							'alias'	=> 'og',
-							'on'	=> 'oi.order_id = og.order_id'
-					)
-			);
+			if ( !empty($keywords)) {
+				$where[] = "( oi.order_sn like '%".$keywords."%' or oi.consignee like '%".$keywords."%' )";
+			}
+			
+			$db_orderinfo_view = RC_Loader::load_app_model('order_info_viewmodel', 'orders');
+			$result = ecjia_app::validate_application('seller');
+			if (!is_ecjia_error($result)) {
+				$db_orderinfo_view->view = array(
+						'order_info' => array(
+								'type'	=> Component_Model_View::TYPE_LEFT_JOIN,
+								'alias'	=> 'oii',
+								'on'	=> 'oi.order_id = oii.main_order_id'
+						),
+						'order_goods' => array(
+								'type'	=> Component_Model_View::TYPE_LEFT_JOIN,
+								'alias'	=> 'og',
+								'on'	=> 'oi.order_id = og.order_id'
+						)
+				);
+			} else {
+				$db_orderinfo_view->view = array(
+						'order_goods' => array(
+								'type'	=> Component_Model_View::TYPE_LEFT_JOIN,
+								'alias'	=> 'og',
+								'on'	=> 'oi.order_id = og.order_id'
+						)
+				);
+			}
+			
+			if ($_SESSION['ru_id'] > 0) {
+				$where['ru_id'] = $_SESSION['ru_id'];
+				$where[] = 'oii.order_id is null';
+			}
+			/* 获取记录条数 */
+			$record_count = $db_orderinfo_view->where($where)->count('oi.order_id');
+			
+			//加载分页类
+			RC_Loader::load_sys_class('ecjia_page', false);
+			//实例化分页
+			$page_row = new ecjia_page($record_count, $size, 6, '', $page);
+			
+			$total_fee = "(oi.goods_amount + oi.tax + oi.shipping_fee + oi.insure_fee + oi.pay_fee + oi.pack_fee + oi.card_fee) as total_fee";
+			
+			$field = 'oi.order_id, oi.order_sn, oi.consignee, oi.mobile, oi.tel, oi.order_status, oi.pay_status, oi.shipping_status, oi.pay_name, '.$total_fee.', oi.integral_money, oi.bonus, oi.shipping_fee, oi.discount, oi.add_time, sum(og.goods_number) as goods_num, og.goods_id';
+			if ($_SESSION['ru_id'] > 0) {
+				$data = $db_orderinfo_view->field($field)->where($where)->group(array('oi.order_id'))->order(array('oi.order_id' => 'desc'))->limit($page_row->limit())->select();
+			} else {
+				$data = $db_orderinfo_view->join(array('order_goods'))->field($field)->where($where)->group(array('oi.order_id'))->order(array('oi.order_id' => 'desc'))->limit($page_row->limit())->select();
+			}
 		} else {
-			$db_orderinfo_view->view = array(
-					'order_goods' => array(
-							'type'	=> Component_Model_View::TYPE_LEFT_JOIN,
-							'alias'	=> 'og',
-							'on'	=> 'oi.order_id = og.order_id'
-					)
-			);
+			
+			
 		}
 		
-		
-		if ($_SESSION['ru_id'] > 0) {
-			$where['ru_id'] = $_SESSION['ru_id'];
-			$where[] = 'oii.order_id is null';
-		}
-		/* 获取记录条数 */
-		$record_count = $db_orderinfo_view->where($where)->count('oi.order_id');
-		
-		//加载分页类
-		RC_Loader::load_sys_class('ecjia_page', false);
-		//实例化分页
-		$page_row = new ecjia_page($record_count, $size, 6, '', $page);
-		
-		$total_fee = "(oi.goods_amount + oi.tax + oi.shipping_fee + oi.insure_fee + oi.pay_fee + oi.pack_fee + oi.card_fee) as total_fee";
-		
-		$field = 'oi.order_id, oi.order_sn, oi.consignee, oi.mobile, oi.tel, oi.order_status, oi.pay_status, oi.shipping_status, oi.pay_name, '.$total_fee.', oi.integral_money, oi.bonus, oi.shipping_fee, oi.discount, oi.add_time, sum(og.goods_number) as goods_num, og.goods_id';
-		if ($_SESSION['ru_id'] > 0) {
-			$data = $db_orderinfo_view->field($field)->where($where)->group(array('oi.order_id'))->order(array('oi.order_id' => 'desc'))->limit($page_row->limit())->select();
-		} else {
-			$data = $db_orderinfo_view->join(array('order_goods'))->field($field)->where($where)->group(array('oi.order_id'))->order(array('oi.order_id' => 'desc'))->limit($page_row->limit())->select();
-		}
-// 		_dump($db_orderinfo_view->last_sql(),1);
 		RC_Lang::load('orders/order');
-// 		_dump($data,1);
+
 		$order_list = array();
 		if (!empty($data)) {
 			$goods_db = RC_Loader::load_app_model('goods_model', 'goods');
@@ -150,42 +155,7 @@ class list_module implements ecjia_interface {
 		
 		EM_Api::outPut($order_list, $pager);
 		
-		
-// 		{
-// 			"order_id": "4125",
-// 			"order_sn": "2015041496313",
-// 			"order_time": "2015/04/14 18:32:12 +0800",
-// 			"total_fee": "1680.00",
-// 			"discount": "0.00",
-// 			"goods_number": "2",
-// 			"goods_list": [
-// 			{
-// 				"goods_id": "108",
-// 				"name": "模板堂精仿也买酒整站模板",
-// 				"goods_number": "2",
-// 				"subtotal": "￥1680.00元",
-// 				"formated_shop_price": "￥840.00元",
-// 				"img": {
-// 				"small": "http://192.168.1.55/ecmoban-admin/content/uploads/images/201203/thumb_img/108_thumb_G_1331074419824.gif",
-// 				"thumb": "http://192.168.1.55/ecmoban-admin/content/uploads/images/201203/goods_img/108_G_1331074419284.jpg",
-// 				"url": "http://192.168.1.55/ecmoban-admin/content/uploads/images/201203/source_img/108_G_1331074419130.jpg"
-// 				}
-// 			}
-// 			],
-// 			"formated_total_fee": "￥1680.00元",
-// 			"formated_integral_money": "￥0.00元",
-// 			"formated_bonus": "￥0.00元",
-// 			"formated_shipping_fee": "￥0.00元",
-// 			"formated_discount": "￥0.00元",
-// 			"order_info": {
-	// 			"pay_code": "pay_alipay",
-	// 			"order_amount": "1680.00",
-	// 			"order_id": "4125",
-	// 			"subject": "模板堂精仿也买酒整站模板等1种商品",
-	// 			"desc": "模板堂精仿也买酒整站模板等1种商品",
-	// 			"order_sn": "2015041496313"
-// 			}
-// 		},
+
 	} 
 }
 

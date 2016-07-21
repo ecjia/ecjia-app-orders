d73b68fdf2b67a09da8336abe204d0e389687f42
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-07-21 15:18:21 +0800

    数据模型修改

--- model/merchants_order_goods_viewmodel.class.php
+++ model/merchants_order_goods_viewmodel.class.php
@@ -11,21 +11,21 @@ class merchants_order_goods_viewmodel extends Component_Model_View {
 		$this->table_alias_name = 'og';
 		
 		$this->view = array(	
+			'order_info' => array(
+					'type'  =>	Component_Model_View::TYPE_LEFT_JOIN,
+					'alias'	=>	'o',
+					'on'    =>	'o.order_id = og.order_id ',
+			),
 			'seller_shopinfo' 	=> array(
 					'type' 		=> Component_Model_View::TYPE_LEFT_JOIN,
 					'alias' 	=> 'ssi',
-					'on' 		=> 'ssi.id = og.seller_id'
+					'on' 		=> 'ssi.id = o.seller_id'
 			),
 		    'merchants_shop_information' 	=> array(
 		        'type' 		=> Component_Model_View::TYPE_LEFT_JOIN,
 		        'alias' 	=> 'ms',
 		        'on' 		=> 'ssi.shop_id = ms.shop_id'
 		    ),
-			'order_info' => array(
-					'type'  =>	Component_Model_View::TYPE_LEFT_JOIN,
-					'alias'	=>	'o',
-					'on'    =>	'o.order_id = og.order_id ',
-			),
 			'users' => array(
 				'type'  	=> Component_Model_View::TYPE_LEFT_JOIN,
 				'alias'		=> 'u',

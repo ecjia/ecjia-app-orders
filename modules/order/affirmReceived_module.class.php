804bd911d9e9e3933ce7d594d17a6e4b1ffb91e1
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-10-19 16:44:19 +0800

    订单确认收货接口 触发结算事件

--- modules/order/affirmReceived_module.class.php
+++ modules/order/affirmReceived_module.class.php
@@ -21,7 +21,7 @@ class affirmReceived_module extends api_front implements api_interface {
 		$result = affirm_received(intval($order_id), $user_id);	
 		if (!is_ecjia_error($result)) {
 		    //update commission_bill
-		    RC_Api::api('commission', 'add_bill_detail', array('order_type' => 2, 'order_id' => $order_id));
+		    RC_Api::api('commission', 'add_bill_detail', array('order_type' => 1, 'order_id' => $order_id));
 		    return array();
 		} else {
 			return new ecjia_error(8, 'fail');

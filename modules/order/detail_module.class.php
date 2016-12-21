ea7a30024e8db49b3dbfde78033429c4e144cd89
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-21 16:10:43 +0800

    新增配送员相关信息

--- modules/order/detail_module.class.php
+++ modules/order/detail_module.class.php
@@ -25,9 +25,9 @@ class detail_module extends api_front implements api_interface {
 		if(is_ecjia_error($order)) {
 		    return $order;
 		}
+		
 		/*返回数据处理*/
 		$order['order_id'] 			= intval($order['order_id']);
-		$order['main_order_id'] 	= intval($order['main_order_id']);
 		$order['user_id'] 			= intval($order['user_id']);
 		$order['order_status'] 		= intval($order['order_status']);
 		$order['shipping_status'] 	= intval($order['shipping_status']);
@@ -44,6 +44,17 @@ class detail_module extends api_front implements api_interface {
 		if ($order === false) {
 			return new ecjia_error(8, 'fail');
 		}
+		
+		/* 判断支付方式*/
+		$shipping_method = RC_Loader::load_app_class('shipping_method', 'shipping');
+		$shipping_info = $shipping_method->shipping_info($order['shipping_id']);
+		$order['shipping_code'] = $shipping_info['shipping_code'];
+		if ($shipping_info['shipping_code'] == 'ship_o2o_express') {
+			$express_info = RC_DB::table('express_order')->where('order_sn', $order['order_sn'])->orderBy('express_id', 'desc')->first();
+			$order['express_user'] = $express_info['express_user'];
+			$order['express_mobile'] = $express_info['express_mobile'];
+		}
+		
 		//收货人地址
 		$db_region = RC_Model::model('shipping/region_model');
 		$region_name = $db_region->in(array('region_id' => array($order['country'], $order['province'], $order['city'], $order['district'])))->order('region_type')->select();
@@ -53,23 +64,16 @@ class detail_module extends api_front implements api_interface {
 		$order['city']		= $region_name[2]['region_name'];
 		$order['district']	= $region_name[3]['region_name'];
 		$goods_list = EM_order_goods($order_id);
-// 		$msi_dbview = RC_Loader::load_app_model('merchants_shop_information_viewmodel', 'seller');
+
 		foreach ($goods_list as $k => $v) {
 			if ($k == 0) {
 				if ($v['store_id'] > 0) {
-					//$field ='msi.user_id, ssi.*, CONCAT(shoprz_brandName,shopNameSuffix) as seller_name';
-					//$seller_info = $msi_dbview->join(array('seller_shopinfo'))
-					//							->field($field)
-					//							->where(array('msi.user_id' => $v['ru_id']))
-					//							->find();
 					$seller_info = RC_DB::table('store_franchisee')->where(RC_DB::raw('store_id'), $v['store_id'])->pluck('merchants_name');
 				}
 
 
 				$order['seller_id']		= isset($v['store_id']) ? intval($v['store_id']) : 0;
 				$order['seller_name']	= isset($seller_info) ? $seller_info : '自营';
-				//$order['store_id']		= isset($v['store_id']) ? intval($v['store_id']) : 0;//后期增加
-				//$order['merchants_name']	= isset($seller_info['merchants_name']) ? $seller_info['merchants_name'] : '自营';  //后期增加
 				$order['service_phone']		= RC_DB::table('merchants_config')->where(RC_DB::raw('store_id'), $v['store_id'])->where(RC_DB::raw('code'), 'shop_kf_mobile')->pluck('value');
 			}
 			$attr = array();
@@ -102,17 +106,17 @@ class detail_module extends api_front implements api_interface {
 		}
 		$order['goods_list'] = $goods_list;
 
-		$db_term_meta = RC_Model::model('goods/goods_term_meta_model');
-		$meta_data_where = array(
-				'object_type'	=> 'ecjia.order',
-				'object_group'	=> 'order',
-				'object_id'		=> $order_id,
-				'meta_key'		=> 'receipt_verification',
-		);
-		$receipt_code = $db_term_meta->where($meta_data_where)->get_field('meta_value');
-		if (!empty($receipt_code)) {
-			$order['receipt_verification'] = $receipt_code;
-		}
+// 		$db_term_meta = RC_Model::model('goods/goods_term_meta_model');
+// 		$meta_data_where = array(
+// 				'object_type'	=> 'ecjia.order',
+// 				'object_group'	=> 'order',
+// 				'object_id'		=> $order_id,
+// 				'meta_key'		=> 'receipt_verification',
+// 		);
+// 		$receipt_code = $db_term_meta->where($meta_data_where)->get_field('meta_value');
+// 		if (!empty($receipt_code)) {
+// 			$order['receipt_verification'] = $receipt_code;
+// 		}
 
 		$order_status_log = RC_Model::model('orders/order_status_log_model')->where(array('order_id' => $order_id))->order(array('log_id' => 'desc'))->select();
 		$order['order_status_log'] = array();
@@ -128,39 +132,9 @@ class detail_module extends api_front implements api_interface {
 				);
 			}
 		}
+		
+		
 
-
-		//支付方式信息
-		$payment_method = RC_Loader::load_app_class('payment_method', 'payment');
-		$payment_info = array();
-		$payment_info = $payment_method->payment_info_by_id($order['pay_id']);
-
-		if ($payment_info['pay_code'] == 'upmp') {
-		    RC_Log::write('upmp get code ' . $payment_info['pay_code'], RC_Log::DEBUG);
-		    if (RC_Loader::load_app_module($payment_info['pay_code'], 'payment', false)) {
-		        $payment = get_payment($payment['pay_code']);
-		        $pay_obj = new $payment_info['pay_code']();
-		        list($resp, $validResp) = $pay_obj->get_tn($order, $payment);
-
-		        // 商户的业务逻辑
-		        if ($validResp){
-		            // 服务器应答签名验证成功
-		            RC_Log::write('upmp get code trade success', RC_Log::DEBUG);
-
-		            if ($resp['respCode'] == '00') {
-		                $order['pay_upmp_tn'] = $resp['tn'];
-		            }
-		            else {
-		                $order['pay_error'] = $resp[respMsg];
-		            }
-		        }
-		        else
-		        {
-		            // 服务器应答签名验证失败
-		            RC_Log::write('upmp get code trade fail', RC_Log::DEBUG);
-		        }
-		    }
-		}
 
 		return array('data' => $order);
 	}

904d76b9947129ae4b0386fb02ee6cea49fc7c0c
靳翔 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-01-16 16:53:11 +0800

    修改商家后台已收货后不能设置未发货

--- functions/merchant_order.func.php
+++ functions/merchant_order.func.php
@@ -149,7 +149,11 @@ function merchant_operable_list($order) {
                         $list['receive'] = true;
                         // 收货确认
                     }
-                    $list['unship'] = true;
+                    if(SS_RECEIVED != $ss) {
+                        $list['unship'] = true;
+                        //已收货后不能设未发货
+                    }
+//                     $list['unship'] = true;
                     // 设为未发货
                     if ($priv_list['os']) {
                         $list['return'] = true;
@@ -194,7 +198,10 @@ function merchant_operable_list($order) {
                         // 收货确认
                     }
                     if (!$is_cod) {
-                        $list['unship'] = true;
+                        if(SS_RECEIVED != $ss) {
+                            $list['unship'] = true;
+                            //已收货后不能设未发货
+                        }
                         // 设为未发货
                     }
                 }

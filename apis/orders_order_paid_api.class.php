86f60520589576217e4d48b0b033917ab6c80ba7
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-06-22 12:10:36 +0800

    更新短信模板变量

--- apis/orders_order_paid_api.class.php
+++ apis/orders_order_paid_api.class.php
@@ -246,7 +246,7 @@ class orders_order_paid_api extends Component_Event_Api {
 	                			'value'  =>array(
 	                				'order_sn'  	=> $order['order_sn'],
                 					'consignee' 	=> $order['consignee'],
-                					'mobile'  		=> $order['mobile'],
+                					'telphone'  	=> $order['mobile'],
                 					'order_amount'	=> $order['order_amount'],
                 					'service_phone' => ecjia::config('service_phone'),
 	                			),

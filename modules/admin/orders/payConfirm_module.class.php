2abf24d7ff83386c1091881add77f1818335aa02
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-22 10:43:37 +0800

    更新admin/order/payConfirm

--- modules/admin/orders/payConfirm_module.class.php
+++ modules/admin/orders/payConfirm_module.class.php
@@ -55,7 +55,7 @@ class payConfirm_module extends api_admin implements api_interface
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request)
     {	
 		$this->authadminSession();
-		if ($_SESSION['admin_id'] <= 0 && $_SESSION['ru_id'] <= 0) {
+		if ($_SESSION['admin_id'] <= 0 && $_SESSION['staff_id'] <= 0) {
 			return new ecjia_error(100, 'Invalid session');
 		}
 		$result = $this->admin_priv('order_stats');
@@ -71,7 +71,6 @@ class payConfirm_module extends api_admin implements api_interface
 		/* 查询订单信息 */
 		$order = RC_Api::api('orders', 'order_info', array('order_id' => $order_id, 'order_sn' => ''));
 		if (empty($order)) {
-			//EM_Api::outPut(13);
 			return new ecjia_error('dose_not_exist', '不存在的信息');
 		}
 		

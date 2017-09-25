0e7b415d2eaa96e31af7300e58bdeffb42c5fb02
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-25 09:11:21 +0800

    提货验证码发送条件修改

--- apis/orders_order_paid_api.class.php
+++ apis/orders_order_paid_api.class.php
@@ -168,10 +168,10 @@ class orders_order_paid_api extends Component_Event_Api {
 		                'add_time'		=> RC_Time::gmtime(),
 	                ));
 	                
-	                /*门店自提，无需物流（收银台）时发送提货验证码；*/
+	                /*门店自提，时发送提货验证码；*/
 	                if ($order['shipping_id'] > 0) {
 	                	$shipping_info = RC_DB::table('shipping')->where('shipping_id', $order['shipping_id'])->first();
-	                	if ($shipping_info['shipping_code'] == 'ship_cac' || $shipping_info['shipping_code'] == 'ship_no_express') {
+	                	if ($shipping_info['shipping_code'] == 'ship_cac') {
 	                		/*短信给用户发送收货验证码*/
 	                		if (ecjia::config('sms_shop_mobile') != '') {
 	                			$db_term_meta = RC_DB::table('term_meta');

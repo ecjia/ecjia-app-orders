b7488de08ae990beb864bf4f9147fee26e34bae9
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-29 14:28:04 +0800

    admin/orders/list查询某个会员的订单

--- modules/admin/orders/list_module.class.php
+++ modules/admin/orders/list_module.class.php
@@ -87,7 +87,7 @@ class list_module extends api_admin implements api_interface {
 		if ($user_id > 0) {
 		    $where['oi.user_id'] = $user_id;
 		}
-		if ($device_code != '8001') {
+		if ($device_code != '8001' || $user_id > 0) {
 			switch ($type) {
 				case 'await_pay':
 					$where_query = $order_query->order_await_pay('oi.');

c356c5e6b619b6e845b3d2466db3f0f278162864
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-10-25 18:50:24 +0800

    去除调试日志

--- apis/orders_buy_order_paid_api.class.php
+++ apis/orders_buy_order_paid_api.class.php
@@ -74,9 +74,6 @@ class orders_buy_order_paid_api extends Component_Event_Api {
 	    if (intval($order['pay_status']) === PS_PAYED) {
 	        return new ecjia_error('order_has_been_paid', '订单已经支付了');
 	    }
-	    RC_Logger::getLogger('error')->info('test1微信支付');
-	    RC_Logger::getLogger('error')->info($order_sn);
-	    RC_Logger::getLogger('error')->info($order);
 	    
 	    /* 改变订单状态 */
         return $this->order_paid($order_sn, $order, PS_PAYED);
@@ -92,7 +89,6 @@ class orders_buy_order_paid_api extends Component_Event_Api {
 	 */
 	private function order_paid($order_sn, $order, $pay_status = PS_PAYED) {
 	    RC_Loader::load_app_func('admin_order', 'orders');
-	    RC_Logger::getLogger('error')->info('test2微信支付');
 	    
 	    $order_id = $order['order_id'];
 	    $order_sn = $order['order_sn'];
@@ -109,8 +105,6 @@ class orders_buy_order_paid_api extends Component_Event_Api {
 	    RC_Logger::getLogger('error')->info($data);
 	    
 	    RC_DB::table('order_info')->where('order_id', $order_id)->update($data);
-	    
-	    RC_Logger::getLogger('error')->info('test3微信支付');
 	    
 	    /* 记录订单操作记录 */
 	    order_action($order_sn, OS_CONFIRMED, SS_UNSHIPPED, $pay_status, '', RC_Lang::get('orders::order.buyers'));

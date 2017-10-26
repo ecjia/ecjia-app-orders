75bc852e55cd843242b6fa471c019fef761a8224
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-10-26 15:17:23 +0800

    添加订单表的新数据模型

---
+++ classes/Repositories/OrdersRepository.php
@@ -0,0 +1,296 @@
+<?php
+//
+//    ______         ______           __         __         ______
+//   /\  ___\       /\  ___\         /\_\       /\_\       /\  __ \
+//   \/\  __\       \/\ \____        \/\_\      \/\_\      \/\ \_\ \
+//    \/\_____\      \/\_____\     /\_\/\_\      \/\_\      \/\_\ \_\
+//     \/_____/       \/_____/     \/__\/_/       \/_/       \/_/ /_/
+//
+//   上海商创网络科技有限公司
+//
+//  ---------------------------------------------------------------------------------
+//
+//   一、协议的许可和权利
+//
+//    1. 您可以在完全遵守本协议的基础上，将本软件应用于商业用途；
+//    2. 您可以在协议规定的约束和限制范围内修改本产品源代码或界面风格以适应您的要求；
+//    3. 您拥有使用本产品中的全部内容资料、商品信息及其他信息的所有权，并独立承担与其内容相关的
+//       法律义务；
+//    4. 获得商业授权之后，您可以将本软件应用于商业用途，自授权时刻起，在技术支持期限内拥有通过
+//       指定的方式获得指定范围内的技术支持服务；
+//
+//   二、协议的约束和限制
+//
+//    1. 未获商业授权之前，禁止将本软件用于商业用途（包括但不限于企业法人经营的产品、经营性产品
+//       以及以盈利为目的或实现盈利产品）；
+//    2. 未获商业授权之前，禁止在本产品的整体或在任何部分基础上发展任何派生版本、修改版本或第三
+//       方版本用于重新开发；
+//    3. 如果您未能遵守本协议的条款，您的授权将被终止，所被许可的权利将被收回并承担相应法律责任；
+//
+//   三、有限担保和免责声明
+//
+//    1. 本软件及所附带的文件是作为不提供任何明确的或隐含的赔偿或担保的形式提供的；
+//    2. 用户出于自愿而使用本软件，您必须了解使用本软件的风险，在尚未获得商业授权之前，我们不承
+//       诺提供任何形式的技术支持、使用担保，也不承担任何因使用本软件而产生问题的相关责任；
+//    3. 上海商创网络科技有限公司不对使用本产品构建的商城中的内容信息承担责任，但在不侵犯用户隐
+//       私信息的前提下，保留以任何方式获取用户信息及商品信息的权利；
+//
+//   有关本产品最终用户授权协议、商业授权与技术服务的详细内容，均由上海商创网络科技有限公司独家
+//   提供。上海商创网络科技有限公司拥有在不事先通知的情况下，修改授权协议的权力，修改后的协议对
+//   改变之日起的新授权用户生效。电子文本形式的授权协议如同双方书面签署的协议一样，具有完全的和
+//   等同的法律效力。您一旦开始修改、安装或使用本产品，即被视为完全理解并接受本协议的各项条款，
+//   在享有上述条款授予的权力的同时，受到相关的约束和限制。协议许可范围以外的行为，将直接违反本
+//   授权协议并构成侵权，我们有权随时终止授权，责令停止损害，并保留追究相关责任的权力。
+//
+//  ---------------------------------------------------------------------------------
+//
+
+namespace Ecjia\App\Orders\Repositories;
+
+use Royalcms\Component\Repository\Repositories\AbstractRepository;
+use ecjia_page;
+
+class OrdersRepository extends AbstractRepository
+{
+    protected $model = 'Ecjia\App\Orders\Models\OrdersModel';
+    
+    protected $orderBy = ['order_info.order_id' => 'desc'];
+    
+    protected $type;
+    
+    public function __construct($type)
+    {
+        parent::__construct();
+        
+        $this->type = $type;
+    }
+    
+    /**
+     * 获取所有的客户端列表
+     * 
+     * @return array
+     */
+    public function getAllClients() {
+        $clients = array(
+            'iPhone' => Client::IPHONE,
+            'Android'=> Client::ANDROID,
+            'H5' 	 => Client::H5,
+            'PC'     => Client::PC
+        );
+        return $clients;
+    }
+   
+    public function findWhereLimit(array $where, $columns = ['*'], $page = 1, $perPage = 15, callable $callback = null)
+    {
+        $this->newQuery();
+        
+        foreach ($where as $field => $value) {
+            if (is_array($value)) {
+                list($field, $condition, $val) = $value;
+                $this->query->where($field, $condition, $val);
+            }
+            else {
+                $this->query->where($field, '=', $value);
+            }
+        }
+        
+        if (is_callable($callback)) {
+            $callback($this->query);
+        }
+        
+        if ($page && $perPage) {
+            $this->query->forPage($page, $perPage);
+        }
+        
+        return $this->query->get($columns);
+    }
+    
+    
+    public function findWhereCount(array $where, $columns = ['*'], callable $callback = null)
+    {
+        $this->newQuery();
+        
+        foreach ($where as $field => $value) {
+            if (is_array($value)) {
+                list($field, $condition, $val) = $value;
+                $this->query->where($field, $condition, $val);
+            }
+            else {
+                $this->query->where($field, '=', $value);
+            }
+        }
+        
+        if (is_callable($callback)) {
+            $callback($this->query);
+        }
+        
+        return $this->query->count();
+    }
+    
+    
+    /**
+     *  获取用户指定范围的订单列表
+     *
+     * @access  public
+     * @param   int         $user_id        用户ID号
+     * @param   int         $num            列表最大数量
+     * @param   int         $start          列表起始位置
+     * @return  array       $order_list     订单列表
+     */
+    public function getUserOrdersList($user_id, $type = null, $page = 1, $size = 15, $keywords = null, $store_id = null)
+    {
+        $where = [
+        	'user_id' => $user_id,
+        	'is_delete' => 0,
+        ];
+        
+        if ($store_id > 0) {
+            $where['store_id'] = $store_id;
+        }
+
+        $field = [
+        	'order_info.order_id',
+        	'order_info.order_sn',
+        	'order_info.order_status',
+        	'order_info.shipping_status',
+        	'order_info.pay_status',
+        	'order_info.add_time',
+        	'order_info.goods_amount',
+        	'order_info.shipping_fee',
+        	'order_info.insure_fee',
+        	'order_info.pay_fee',
+        	'order_info.pack_fee',
+        	'order_info.card_fee',
+        	'order_info.tax',
+        	'order_info.integral_money',
+        	'order_info.bonus',
+        	'order_info.discount',
+        	'order_info.pay_id',
+        	'order_info.order_amount',
+        ];
+        
+        if (!empty($keywords)) {
+            $field[] = 'order_goods.goods_id';
+            $field[] = 'order_goods.goods_name';
+        }
+        
+        $count = $this->findWhereCount($where, [], function($query) use ($keywords) {
+            if (!empty($keywords)) {
+                $query->where(function ($query) use ($keywords) {
+                    return $query->where('order_goods.goods_name', 'like', '%' . $keywords .'%')
+                          ->orWhere('order_info.order_sn', 'like', '%' . $keywords .'%');
+                });
+                
+                $query->leftJoin('order_goods', function ($join) {
+                    $join->on('order_info.order_id', '=', 'order_goods.order_id');
+                });
+                
+                $query->groupby('order_info.order_id');
+            }
+        });
+        
+        $orders = $this->findWhereLimit($where, $field, $page, $size, function($query) use ($keywords) {
+            $query->with('goods', 'goods.goods', 'store');
+            
+            if (!empty($keywords)) {
+                $query->leftJoin('order_goods', function ($join) {
+                    $join->on('order_info.order_id', '=', 'order_goods.order_id');
+                });
+                    
+                $query->where(function ($query) use ($keywords) {
+                    $query->where('order_goods.goods_name', 'like', '%' . $keywords .'%')
+                          ->orWhere('order_info.order_sn', 'like', '%' . $keywords .'%');
+                });
+                
+                $query->groupby('order_info.order_id');
+            }
+        });
+        
+        $orders->map(function ($item) {
+//             dd($item->goods);
+//             $item['goods'] = $item->goods;
+//             return $item;
+//             $goods = $item->goods()->with(['goods' => function ($query) {
+//             	$query->select('goods_thumb', 'original_img', 'goods_img')->get();
+//             }])->get();
+            
+//             dd($goods);
+
+            $data = [
+            	'seller_id' => '',
+            	'seller_name' => '',
+            	'manage_mode' => '',
+                
+                'order_id' => '',
+                'order_sn' => '',
+                'order_status' => '',
+                'shipping_status' => '',
+                'pay_status' => '',
+                'label_order_status' => '',
+                'order_status_code' => '',
+                'order_time' => '',
+                'total_fee' => '',
+                'discount' => '',
+                'goods_number' => '',
+                'is_cod' => '',
+                'formated_total_fee' => '',
+                'formated_integral_money' => '',
+                'formated_bonus' => '',
+                'formated_shipping_fee' => '',
+                'formated_discount' => '',
+                
+                'order_info' => [
+            	   'pay_code' => '',
+            	   'order_amount' => '',
+            	   'order_id' => '',
+            	   'subject' => '',
+            	   'desc' => '',
+            	   'order_sn' => '',
+                ],
+                
+                'goods_list' => [],
+            ];
+            
+            $item['goods_list'] = $item->goods->map(function ($item) {
+                $attr = array();
+                if (!empty($item->goods_attr)) {
+                    $goods_attr = explode("\n", $item['goods_attr']);
+                    $goods_attr = array_filter($goods_attr);
+                    foreach ($goods_attr as  $val) {
+                        $a = explode(':', $val);
+                        if (!empty($a[0]) && !empty($a[1])) {
+                            $attr[] = array('name' => $a[0], 'value' => $a[1]);
+                        }
+                    }
+                }
+                
+//                 $item->goods_attr = $attr;
+                
+                $data = [
+                	'goods_id' => $item->goods_id,
+                	'name' => $item->goods_name,
+                	'goods_attr_id' => $item->goods_attr_id,
+                	'goods_attr' => $attr,
+                	'goods_number' => '',
+                	'subtotal' => '',
+                	'formated_shop_price' => '',
+                    'img' => [
+                    	'small' => '',
+                    	'thumb' => '',
+                    	'url' => '',
+                    ],
+                    'is_commented' => '',
+                    'is_showorder' => '',
+                ];
+                
+                return $data;
+            });
+
+            
+        });
+        
+        dd($orders);
+        
+    }
+    
+}

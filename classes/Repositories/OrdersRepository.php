e74ab4719b229b143ebe1eb47e82bcf83e258ec2
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-10-27 22:16:31 +0800

    优化订单查询封装

--- classes/Repositories/OrdersRepository.php
+++ classes/Repositories/OrdersRepository.php
@@ -286,12 +286,12 @@ class OrdersRepository extends AbstractRepository
                 ];
                 
                 return $data;
-            });
+            })->toArray();
 
             return $data;
         });
         
-//         dd($orderlist->toArray());
+        dd($orderlist->toArray());
 //         dd($orders);
         
         return array('order_list' => $orderlist->toArray(), 'count' => $count);

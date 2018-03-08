5641ac44c62c34f49bbcd89f4587567032c9ae1d
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-03-08 16:03:35 +0800

    商家进行退货退款处理更新

--- merchant.php
+++ merchant.php
@@ -3620,7 +3620,6 @@ class merchant extends ecjia_merchant {
 		RC_Loader::load_app_class('order_refund', 'refund', false);
 		$action_note = $_POST['action_note'];
 		
-		
 		$refund_type = trim($_POST['refund_type']);
 		$refund_reason = intval($_POST['refund_reason']);
 		$order_id	= intval($_POST['order_id']);
@@ -3653,12 +3652,12 @@ class merchant extends ecjia_merchant {
 			
 		//退款编号
 		$refund_sn = order_refund::get_refund_sn();
-		
-		//退货状态
-		if ($refund_type == 'refund') {
+		if ($refund_type == 'refund') {//仅退款
 			$return_status = 0;
-		} elseif ($refund_type == 'return') {
+			$refund_status = 1;
+		} elseif ($refund_type == 'return') {//退货退款
 			$return_status = 1;
+			$refund_status = 0;
 		}
 		
 		/* 进入售后 */
@@ -3691,10 +3690,11 @@ class merchant extends ecjia_merchant {
 				'inv_tax'		=> $order['tax'],
 				'order_amount'	=> $order['order_amount'],
 				'money_paid'	=> $order['money_paid'],
-				'status'		=> 0,
+				'status'		=> 1,
+				'refund_status'	=> $refund_status,
+				'return_status'	=> $return_status,
 				'refund_content'=> $refund_content,
 				'refund_reason'	=> $refund_reason,
-				'return_status'	=> $return_status,
 				'add_time'		=> RC_Time::gmtime(),
 				'referer'		=> 'admin'
 		);
@@ -3734,19 +3734,77 @@ class merchant extends ecjia_merchant {
 		
 		
 		//仅退款---同意---进入打款表
+		$refund_info = RC_DB::table('refund_order')->where('refund_id', $refund_id)->first();
+		if($refund_type == 'refund') {
+			$payment_record_id = RC_DB::TABLE('payment_record')->where('order_sn', $refund_info['order_sn'])->pluck('id');
+				
+			//实际支付费用
+			$order_money_paid = $refund_info['surplus'] + $refund_info['money_paid'];
+			//退款总金额
+			$shipping_status = RC_DB::TABLE('order_info')->where('order_id', $refund_info['order_id'])->pluck('shipping_status');
+			if ($shipping_status > SS_UNSHIPPED) {
+				$back_money_total  = $refund_info['money_paid'] + $refund_info['surplus'] - $refund_info['pay_fee'] - $refund_info['shipping_fee'] - $refund_info['insure_fee'];
+				$back_shipping_fee = $refund_info['shipping_fee'];
+				$back_insure_fee   = $refund_info['insure_fee'];
+			} else {
+				$back_money_total  = $refund_info['money_paid'] + $refund_info['surplus'] - $refund_info['pay_fee'];
+				$back_shipping_fee = 0;
+				$back_insure_fee   = 0;
+			}
+			$data = array(
+					'store_id'	=>	$_SESSION['store_id'],
+					'order_id'	=>	$refund_info['order_id'],
+					'order_sn'	=>	$refund_info['order_sn'],
+					'refund_id'	=>	$refund_info['refund_id'],
+					'refund_sn'	=>	$refund_info['refund_sn'],
+					'refund_type'		=>	$refund_info['refund_type'],
+					'goods_amount'		=>	$refund_info['goods_amount'],
+					'back_pay_code'		=>	$refund_info['pay_code'],
+					'back_pay_name'		=>	$refund_info['pay_name'],
+					'back_pay_fee'		=>	$refund_info['pay_fee'],
+					'back_shipping_fee'	=>	$back_shipping_fee,
+					'back_insure_fee'	=>	$back_insure_fee,
+					'back_pack_id'	=>	$refund_info['pack_id'],
+					'back_pack_fee'	=>	$refund_info['pack_fee'],
+					'back_card_id'	=>	$refund_info['card_id'],
+					'back_card_fee'	=>	$refund_info['card_fee'],
+					'back_bonus_id'	=>	$refund_info['bonus_id'],
+					'back_bonus'	=>	$refund_info['bonus'],
+					'back_surplus'	=>  $refund_info['surplus'],
+					'back_integral'	=>  $refund_info['integral'],
+					'back_integral_money'	=> $refund_info['integral_money'],
+					'back_inv_tax'			=> $refund_info['inv_tax'],
+					'order_money_paid'		=> $order_money_paid,
+					'back_money_total'		=> $back_money_total,
+					'payment_record_id'		=> $payment_record_id,
+					'add_time'	=> RC_Time::gmtime()
+			);
+			RC_DB::table('refund_payrecord')->insertGetId($data);
+		} else {//退货退款---同意---进入待买家发货
+			RC_DB::table('refund_order')->where('refund_id', $refund_id)->update(array('status' => $status, 'return_shipping_range' => $return_shipping_range));
+		}
 		
-		//退货退款---同意---进入待买家发货
+		//录入退款操作日志表
+		$data = array(
+				'refund_id' 		=> $refund_id,
+				'action_user_type'	=>	'merchant',
+				'action_user_id'	=>  $_SESSION['staff_id'],
+				'action_user_name'	=>	$_SESSION['staff_name'],
+				'status'		    =>  1,
+				'refund_status'		=>  $refund_status,
+				'return_status'		=>  $refund_status,
+				'action_note'		=>  $merchant_action_note,
+				'log_time'			=>  RC_Time::gmtime(),
+		);
+		RC_DB::table('refund_order_action')->insertGetId($data);
 		
-		
-		
-		
-		
-		
-		
-		
+		//售后订单状态变动日志表
+		RefundStatusLog::refund_order_process(array('refund_id' => $refund_id, 'status' => 1));
+		//普通订单状态变动日志表
+		OrderStatusLog::refund_order_process(array('order_id' => $refund_info['order_id'], 'status' => 1));
 		
 		/* 操作成功 */
-		return $this->showmessage('操作成功', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_SUCCESS,array('pjaxurl' => RC_Uri::url('order/merchant/info', array('order_id' => $order_id))));
+		return $this->showmessage('申请操作成功', ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_SUCCESS,array('pjaxurl' => RC_Uri::url('order/merchant/info', array('order_id' => $order_id))));
 	}
 }
 

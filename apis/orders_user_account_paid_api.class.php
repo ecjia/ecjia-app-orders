aeee4e2534cb7a5a74b7f7da7d8cc8ac50072255
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-03-20 17:10:42 +0800

    发送短信判断注释

--- apis/orders_user_account_paid_api.class.php
+++ apis/orders_user_account_paid_api.class.php
@@ -154,7 +154,7 @@ class orders_user_account_paid_api extends Component_Event_Api {
 			$shipping_info = RC_DB::table('shipping')->where('shipping_id', $order_info['shipping_id'])->first();
 			if ($shipping_info['shipping_code'] == 'ship_cac') {
 				/*短信给用户发送收货验证码*/
-				if (ecjia::config('sms_shop_mobile') != '') {
+// 				if (ecjia::config('sms_shop_mobile') != '') {
 					$db_term_meta = RC_DB::table('term_meta');
 					$max_code = $db_term_meta->where('object_type', 'ecjia.order')->where('object_group', 'order')->where('meta_key', 'receipt_verification')->max('meta_value');
 					 
@@ -181,7 +181,7 @@ class orders_user_account_paid_api extends Component_Event_Api {
 							'meta_value'	=> $code,
 					);
 					$db_term_meta->insert($meta_data);
-				}
+// 				}
 			}
 		}
 		

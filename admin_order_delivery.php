f666c127acbb7c1e644fc24ef1996df77bcbd89f
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-05-11 10:00:08 +0800

    订单退款，用户下单有赠送的积分的话则扣除

--- admin_order_delivery.php
+++ admin_order_delivery.php
@@ -420,12 +420,16 @@ class admin_order_delivery extends ecjia_admin {
 				$integral = integral_to_give($order);
 
 				$options = array(
-					'user_id'		=> $order['user_id'],
-					'rank_points'	=> intval($integral['rank_points']),
-					'pay_points'	=> intval($integral['custom_points']),
-					'change_desc'	=> sprintf(RC_Lang::get('orders::order.order_gift_integral'), $order['order_sn'])
+					'user_id'			=> $order['user_id'],
+					'rank_points'		=> intval($integral['rank_points']),
+					'pay_points'		=> intval($integral['custom_points']),
+					'change_desc'		=> sprintf(RC_Lang::get('orders::order.order_gift_integral'), $order['order_sn']),
+					'order_sn'			=> $order['order_sn'],
+					'from_type'			=> 'order_give_integral',
+					'from_value'		=> $order['order_sn']
 				);
 				RC_Api::api('user', 'account_change_log',$options);
+				
 				/* 发放红包 */
 				send_order_bonus($order_id);
 			}

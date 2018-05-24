5da7a72235c28ca10d2862f8765e35cc8a92605d
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-05-24 16:49:53 +0800

    多余文件删除

--- admin_order_delivery.php
+++ admin_order_delivery.php
@@ -763,7 +763,7 @@ class admin_order_delivery extends ecjia_admin {
             } else {
                 $express_id = RC_DB::table('express_order')->insertGetId($express_data);
             }
-            /*配送单生成后，自动派单。只有订单配送方式是众包配送时才去自动派单*/
+            /*配送单生成后，自动派单。只有订单配送方式是众包配送和商家配送时才去自动派单*/
             if ($shipping_info['shipping_code'] == 'ship_ecjia_express' && empty($staff_id)) {
             	$params = array(
             			'express_id' => $express_id,

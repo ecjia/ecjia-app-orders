02a671733c2357bf0f7a220dd320842915dc5cf2
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-31 15:47:55 +0800

    提交后台首页样式模版

---
+++ templates/admin/library/widget_admin_dashboard_shopstats_right.lbi.php
@@ -0,0 +1,69 @@
+<div class="move-mod-group" id="widget_admin_dashboard_express_stats">
+	<div class="heading clearfix move-mod-head">
+		<h3 class="pull-left">配送调度统计</h3>
+	</div>
+	<div class="move-mod-content">
+		<div class="mod-content-item">
+			<div class="title">待派单（单）</div>
+			<div class="num">17</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">待取货（单）</div>
+			<div class="num">18</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">配送中（单）</div>
+			<div class="num">19</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">总配送单（单）</div>
+			<div class="num">20</div>
+		</div>
+	</div>
+</div>
+<div class="move-mod-group" id="widget_admin_dashboard_promotion_stats">
+	<div class="heading clearfix move-mod-head">
+		<h3 class="pull-left">促销活动</h3>
+	</div>
+	<div class="move-mod-content">
+		<div class="mod-content-item">
+			<div class="title">促销（个）</div>
+			<div class="num">21</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">优惠（个）</div>
+			<div class="num">22</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">团购（个）</div>
+			<div class="num">23</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">买单（个）</div>
+			<div class="num">24</div>
+		</div>
+	</div>
+</div>
+<div class="move-mod-group" id="widget_admin_dashboard_article_stats">
+	<div class="heading clearfix move-mod-head">
+		<h3 class="pull-left">文章统计情况</h3>
+	</div>
+	<div class="move-mod-content">
+		<div class="mod-content-item">
+			<div class="title">今日新增（篇）</div>
+			<div class="num">25</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">待审核文章（篇）</div>
+			<div class="num">26</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">带审核评论（条）</div>
+			<div class="num">27</div>
+		</div>
+		<div class="mod-content-item">
+			<div class="title">总文章数（篇）</div>
+			<div class="num">28</div>
+		</div>
+	</div>
+</div>

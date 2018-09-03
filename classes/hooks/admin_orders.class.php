417365b03e4cbea83939c1969eef8805b09bc846
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-03 10:26:41 +0800

    更新后台首页订单统计排序

--- classes/hooks/admin_orders.class.php
+++ classes/hooks/admin_orders.class.php
@@ -374,8 +374,8 @@ class orders_admin_plugin
 // RC_Hook::add_action('admin_dashboard_top', array('orders_admin_plugin', 'widget_admin_dashboard_shopchart'));
 RC_Hook::add_action('admin_dashboard_top', array('orders_admin_plugin', 'widget_admin_dashboard_shopstats'));
 
-RC_Hook::add_action('admin_dashboard_left', array('orders_admin_plugin', 'widget_admin_dashboard_shopstats_left'));
-RC_Hook::add_action('admin_dashboard_right', array('orders_admin_plugin', 'widget_admin_dashboard_shopstats_right'));
+RC_Hook::add_action('admin_dashboard_left', array('orders_admin_plugin', 'widget_admin_dashboard_shopstats_left'), 9);
+RC_Hook::add_action('admin_dashboard_right', array('orders_admin_plugin', 'widget_admin_dashboard_shopstats_right'), 9);
 
 RC_Hook::add_action('admin_dashboard_left', array('orders_admin_plugin', 'widget_admin_dashboard_ordersstat'));
 RC_Hook::add_action('admin_dashboard_left', array('orders_admin_plugin', 'widget_admin_dashboard_orderslist'));

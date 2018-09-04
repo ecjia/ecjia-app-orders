7d484c1c79c142878415eb7ade60ec14df8db952
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-04 13:50:25 +0800

    修改js

--- statics/js/order_stats.js
+++ statics/js/order_stats.js
@@ -1,43 +1,46 @@
 // JavaScript Document
 ;
 (function (app, $) {
-    app.order_stats = {
-        init: function () {
-            app.order_stats.searchForm();
-            app.order_stats.order_stats();
-        },
-        
-        searchForm: function () {
-            $('.screen-btn').off('click').on('click', function (e) {
-                e.preventDefault();
-                var year = $("select[name='year']").val(); //开始时间
-                var month = $("select[name='month']").val(); //结束时间
-                var url = $("form[name='searchForm']").attr('action'); //请求链接
-                
-                if (year == 0 || year == undefined) {
-                	ecjia.admin.showmessage({'state': 'error', 'message': '请选择年份'});
-                	return false;
-                }
-                url += '&year=' + year;
+	app.order_stats = {
+		init: function () {
+			app.order_stats.searchForm();
+			app.order_stats.order_stats();
+		},
 
-                if (month != undefined && month != 0) {
-                	url += '&month=' + month;
-                }
-                ecjia.pjax(url);
-            });
-            
-            $('.search-btn').off('click').on('click', function (e) {
-                e.preventDefault();
-                var keywords = $("input[name='keywords']").val();
-                var url = $("form[name='searchForm']").attr('action'); //请求链接
-                
-                if (keywords != '' && keywords != undefined) {
-                	url += '&keywords=' + keywords;
-                }
-                ecjia.pjax(url);
-            });
-        },
-        
+		searchForm: function () {
+			$('.screen-btn').off('click').on('click', function (e) {
+				e.preventDefault();
+				var year = $("select[name='year']").val(); //开始时间
+				var month = $("select[name='month']").val(); //结束时间
+				var url = $("form[name='searchForm']").attr('action'); //请求链接
+
+				if (year == 0 || year == undefined) {
+					ecjia.admin.showmessage({
+						'state': 'error',
+						'message': '请选择年份'
+					});
+					return false;
+				}
+				url += '&year=' + year;
+
+				if (month != undefined && month != 0) {
+					url += '&month=' + month;
+				}
+				ecjia.pjax(url);
+			});
+
+			$('.search-btn').off('click').on('click', function (e) {
+				e.preventDefault();
+				var keywords = $("input[name='keywords']").val();
+				var url = $("form[name='searchForm']").attr('action'); //请求链接
+
+				if (keywords != '' && keywords != undefined) {
+					url += '&keywords=' + keywords;
+				}
+				ecjia.pjax(url);
+			});
+		},
+
 		order_stats: function () {
 			var dataset = [];
 			var ticks = [];
@@ -75,18 +78,18 @@
 								if (stats == 'valid_order') {
 									return params.name + ' ：  ' + params.value + '单';
 								} else {
-									return params.name + ' ：  ' + '￥'+params.value;
+									return params.name + ' ：  ' + '￥' + params.value;
 								}
 							}
 						},
 					},
 					grid: {
-				        left: '2%',
-				        right: '2%',
-				        bottom: '5%',
-				        top: '5%',
-				        containLabel: true
-				    },
+						left: '2%',
+						right: '2%',
+						bottom: '5%',
+						top: '5%',
+						containLabel: true
+					},
 					series: [{
 						data: dataset,
 						type: 'bar',
@@ -95,14 +98,14 @@
 				};
 
 				orderStatsChart.setOption(option);
-				
-				window.addEventListener("resize", function() { 
-					orderStatsChart.resize();  
+
+				window.addEventListener("resize", function () {
+					orderStatsChart.resize();
 				});
 			}
 		},
-    };
-    
+	};
+
 })(ecjia.admin, jQuery);
- 
+
 // end

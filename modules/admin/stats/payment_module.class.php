54820ee9c761c067507c020fd8361101a6676073
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-15 16:22:16 +0800

    修复订单模块API

--- modules/admin/stats/payment_module.class.php
+++ modules/admin/stats/payment_module.class.php
@@ -81,38 +81,37 @@ class payment_module extends api_admin implements api_interface
 		$data = null;
 		if (empty($data)) {
 			$device =  $this->device;
-			$response = payment_stats($start_date, $end_date, $device);
+			$response = $this->payment_stats($start_date, $end_date, $device);
 			RC_Cache::app_cache_set($cache_key, $response, 'stats', 60);
 		} else {
 			$response = $data;
 		}
 		return $response;
 	}
-	 
-}
-function payment_stats($start_date, $end_date, $device)
-{
-	$type = $start_date == $end_date ? 'time' : 'day';
-	$start_date = RC_Time::local_strtotime($start_date. ' 00:00:00');
-	$end_date	= RC_Time::local_strtotime($end_date. ' 23:59:59');
 
-	/* 获取请求当前数据的device信息*/
-	$codes = array('8001', '8011');
-	if (!is_array($device) || !isset($device['code']) || !in_array($device['code'], $codes)) {
-		return new ecjia_error('caskdesk_error', '非收银台请求！');
-	}
-	
-	/* 获取收银台的固有支付方式*/
-	$cashdesk_payment	= array('pay_cash', 'pay_koolyun_alipay', 'pay_koolyun_unionpay', 'pay_koolyun_wxpay', 'pay_balance');
-	$payment_where		= array('enabled' => 1, 'pay_code' => $cashdesk_payment);
-	$pay_id_group		= RC_Model::model('payment/payment_model')->where($payment_where)->get_field('pay_code, pay_id, pay_name', true);
-	
-	/* 定义默认数据*/
-	$data = array();
-	
-	$field = 'count(*) as count, SUM((goods_amount - discount + tax + shipping_fee + insure_fee + pay_fee + pack_fee + card_fee)) AS total_fee';
-	foreach ($cashdesk_payment as $val) {
-		if (isset($pay_id_group[$val])) {
+    private function payment_stats($start_date, $end_date, $device)
+    {
+        $type = $start_date == $end_date ? 'time' : 'day';
+        $start_date = RC_Time::local_strtotime($start_date. ' 00:00:00');
+        $end_date	= RC_Time::local_strtotime($end_date. ' 23:59:59');
+
+        /* 获取请求当前数据的device信息*/
+        $codes = array('8001', '8011');
+        if (!is_array($device) || !isset($device['code']) || !in_array($device['code'], $codes)) {
+            return new ecjia_error('caskdesk_error', '非收银台请求！');
+        }
+
+        /* 获取收银台的固有支付方式*/
+        $cashdesk_payment	= array('pay_cash', 'pay_koolyun_alipay', 'pay_koolyun_unionpay', 'pay_koolyun_wxpay', 'pay_balance');
+        $payment_where		= array('enabled' => 1, 'pay_code' => $cashdesk_payment);
+        $pay_id_group		= RC_Model::model('payment/payment_model')->where($payment_where)->get_field('pay_code, pay_id, pay_name', true);
+
+        /* 定义默认数据*/
+        $data = array();
+
+        $field = 'count(*) as count, SUM((goods_amount - discount + tax + shipping_fee + insure_fee + pay_fee + pack_fee + card_fee)) AS total_fee';
+        foreach ($cashdesk_payment as $val) {
+            if (isset($pay_id_group[$val])) {
 // 			$order_stats = RC_Model::model('orders/cashier_record_viewmodel')->join(array('order_info', 'staff_user'))
 // 														->field($field)
 // 														->where(
@@ -125,25 +124,28 @@ function payment_stats($start_date, $end_date, $device)
 // 															)
 // 														)
 // 		    ->find();
-// 							
-            $order_stats = RC_DB::table('cashier_record as cr')
-    			->leftJoin('order_info as oi', RC_DB::raw('cr.order_id'), '=', RC_DB::raw('oi.order_id'))
-    			->select('count(*) as count', 'SUM((goods_amount - discount + tax + shipping_fee + insure_fee + pay_fee + pack_fee + card_fee)) AS total_fee')
-    			->where(RC_DB::raw('oi.pay_status'), 2)
-    			->whereBetween(RC_DB::raw('oi.pay_time'), array($start_date, $end_date))
-    			->where(RC_DB::raw('cr.staff_id'), $_SESSION['staff_id'])
-    			->where(RC_DB::raw('cr.mobile_device_id'), $_SESSION['device_id'])
-    			->where('pay_id', $pay_id_group[$val]['pay_id'])
-    			->first();
-			$data[] = array(
-					'pay_code'		=> $val,
-					'pay_name'		=> $pay_id_group[$val]['pay_name'],
-					'order_count'	=> $order_stats['count'],
-					'order_amount'	=> empty($order_stats['total_fee']) ? '0.00' : $order_stats['total_fee'],
-					'formatted_order_amount' => empty($order_stats['total_fee']) ? '0.00' : price_format($order_stats['total_fee']),
-			); 												
-		}
-	}
-	
-	return $data;
+//
+                $order_stats = RC_DB::table('cashier_record as cr')
+                    ->leftJoin('order_info as oi', RC_DB::raw('cr.order_id'), '=', RC_DB::raw('oi.order_id'))
+                    ->select('count(*) as count', 'SUM((goods_amount - discount + tax + shipping_fee + insure_fee + pay_fee + pack_fee + card_fee)) AS total_fee')
+                    ->where(RC_DB::raw('oi.pay_status'), 2)
+                    ->whereBetween(RC_DB::raw('oi.pay_time'), array($start_date, $end_date))
+                    ->where(RC_DB::raw('cr.staff_id'), $_SESSION['staff_id'])
+                    ->where(RC_DB::raw('cr.mobile_device_id'), $_SESSION['device_id'])
+                    ->where('pay_id', $pay_id_group[$val]['pay_id'])
+                    ->first();
+                $data[] = array(
+                    'pay_code'		=> $val,
+                    'pay_name'		=> $pay_id_group[$val]['pay_name'],
+                    'order_count'	=> $order_stats['count'],
+                    'order_amount'	=> empty($order_stats['total_fee']) ? '0.00' : $order_stats['total_fee'],
+                    'formatted_order_amount' => empty($order_stats['total_fee']) ? '0.00' : price_format($order_stats['total_fee']),
+                );
+            }
+        }
+
+        return $data;
+    }
+	 
 }
+

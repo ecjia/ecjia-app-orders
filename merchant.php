1ae9d363a506d774b01f44d32215bafc385c9fda
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-29 11:12:04 +0800

    未使用数据模型移除

--- merchant.php
+++ merchant.php
@@ -63,7 +63,6 @@ class merchant extends ecjia_merchant
     private $db_user_address;
     private $db_bonus;
     private $db_order_goodview;
-    //private $db_shipping;
     private $db_delivery;
     private $db_goods;
     private $db_products;
@@ -95,7 +94,6 @@ class merchant extends ecjia_merchant
         $this->db_user_address = RC_Model::model('user/user_address_viewmodel');
         $this->db_bonus = RC_Model::model('orders/bonus_type_user_viewmodel');
         $this->db_order_goodview = RC_Model::model('orders/order_order_goods_viewmodel');
-        //$this->db_shipping = RC_Model::model('shipping/shipping_model');
         $this->db_delivery = RC_Model::model('orders/delivery_goods_model');
         $this->db_goods = RC_Model::model('goods/goods_model');
         $this->db_products = RC_Model::model('goods/products_model');

c9ebe4a381303e28446a31f32e28b6d9bb3c7833
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-10-26 14:43:11 +0800

    order/affirmReceived修改

--- modules/order/affirmReceived_module.class.php
+++ modules/order/affirmReceived_module.class.php
@@ -158,7 +158,7 @@ class order_affirmReceived_module extends api_front implements api_interface {
                                                             ->leftJoin('order_info as oi', RC_DB::raw('eo.order_id'), '=', RC_DB::raw('oi.order_id'));
                             $express_order_viewdb ->where(RC_DB::raw('eo.staff_id'), $val['staff_id'])->where(RC_DB::raw('eo.express_id'), $val['express_id']);
                             $field = 'eo.*, oi.add_time as order_time, oi.pay_time, oi.order_amount, oi.pay_name, oi.shipping_id, oi.invoice_no, sf.merchants_name, sf.address as merchant_address, sf.longitude as merchant_longitude, sf.latitude as merchant_latitude';
-                            $express_order_info = $express_order_viewdb->select(RC_DB::raw($filed))->first();
+                            $express_order_info = $express_order_viewdb->select(RC_DB::raw($field))->first();
 
                             //短信发送
                             if (!empty($express_order_info['express_mobile'])) {

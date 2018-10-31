244a0ab5f6b4e3640874bc6116ecde49a85bb927
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-10-31 15:18:56 +0800

    方法移除

--- functions/admin_order.func.php
+++ functions/admin_order.func.php
@@ -866,15 +866,7 @@ function order_bonus($order_id) {
     }
     return $list;
 }
-/**
-* 得到新发货单号
-* @return  string
-*/
-function get_delivery_sn() {
-    /* 选择一个随机的方案 */
-    mt_srand((double) microtime() * 1000000);
-    return date('YmdHi') . str_pad(mt_rand(1, 99999), 5, '0', STR_PAD_LEFT);
-}
+
 /**
  * 记录订单操作记录
  *

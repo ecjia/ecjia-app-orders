379c5290ef1e9b019a774e540b62a7cb220a6ebb
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-11 11:15:35 +0800

    修改PayConstant::PAYCODE_前缀的常量为\Ecjia\App\Payment\Enums\PayCodeEnum枚举类

--- modules/order/pay_module.class.php
+++ modules/order/pay_module.class.php
@@ -95,7 +95,7 @@ class order_pay_module extends api_front implements api_interface
         $handler->set_orderinfo($order);
         $handler->set_mobile($is_mobile);
 
-        $result = $handler->get_code(Ecjia\App\Payment\PayConstant::PAYCODE_PARAM);
+        $result = $handler->get_code(\Ecjia\App\Payment\Enums\PayCodeEnum::PAYCODE_PARAM);
         if (is_ecjia_error($result)) {
             return $result;
         } else {
